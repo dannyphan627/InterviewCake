c63ac4dee704de6f7d7c05ee5bb67dca011b4d62
dannyphan627 <59778516+dannyphan627@users.noreply.github.com>
2020-07-21 19:04:36 -0700

    Create deleteNode.js

---
+++ deleteNode.js
@@ -0,0 +1,19 @@
+class LinkedListNode {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+function deleteNode(nodeToDelete) {
+
+  // Delete the input node from the linked list
+  const nextNode = nodeToDelete.next;
+  if(nextNode){
+    nodeToDelete.value = nextNode.value;
+  nodeToDelete.next = nextNode.next;
+  } else {
+    throw new Error('Can not delete last node with this technique');
+  }
+}
+
